a24ffc70ab6316af3b0721ca2ca40077cd348bce
johnhom1024 <924358746@qq.com>
2023-04-03 22:55:32 +0800

    feat: 使用cli-shared-utils进行优化

--- packages/mg-cli/src/utils/merge.ts
+++ packages/mg-cli/src/utils/merge.ts
@@ -1,5 +1,12 @@
 import shell from 'shelljs';
-import { log, info, error } from '@johnhom/cli-shared-utils';
+import {
+  log,
+  info,
+  error,
+  done,
+  logWithSpinner,
+  stopSpinner,
+} from '@johnhom/cli-shared-utils';
 // 命令结果不输出到终端
 shell.config.silent = false;
 // 设置第一次出错时退出，如果这里不设置，则不会触发reject
@@ -13,7 +20,7 @@ async function getCurrentBranchName() {
     const { stdout } = result;
     // 这里存在尾换行符
     const currentBranch = stdout.trim();
-    console.log('你当前的功能分支是：', currentBranch);
+    info(`当前的分支是：${currentBranch}`);
     return currentBranch;
   } catch (error) {
     throw error;
@@ -25,17 +32,19 @@ async function pullTargetBranch(targetBranch: string): Promise<void> {
     const { stdout } = await shell.exec('git remote');
     if (stdout) {
       // 执行pull
-      console.log('执行下拉...');
+      logWithSpinner(`下拉${targetBranch}分支的代码...`);
       await shell.exec('git pull');
+      stopSpinner();
+      done('下拉完成');
     }
   } catch (error) {
-    console.log('目标分支没有远程分支');
+    info('目标分支没有远程分支');
   }
 }
 
 export async function mergeBranch({ target = 'dev', needPush = false }) {
   if (!shell.which('git')) {
-    shell.echo('Sorry, this script requires git');
+    error('Sorry, this script requires git');
     shell.exit(1);
   }
 
@@ -44,51 +53,55 @@ export async function mergeBranch({ target = 'dev', needPush = false }) {
 
   // 这里判断当前分支和目标分支的名称是否一致
   if (sourceBranch === target) {
-    console.log('当前分支与目标分支是同一个分支');
+    error('当前分支与目标分支是同一个分支');
     return;
   }
 
   // 切换到目标分支
-  console.log(`切换到目标分支：\n ${target}`);
+  info(`准备切换到目标分支：${target}`);
 
   try {
     await shell.exec(`git checkout ${target}`);
-    console.log(`已切到分支：${target}`);
-  } catch (error) {
+    info(`已切到分支：${target}`);
+  } catch (errorInstance) {
     // 不存在该分支
-    console.error(`切换到${target}分支时出现错误，请检查是否存在该分支`);
+    error(`切换到${target}分支时出现错误，请检查是否存在该分支`);
     return;
   }
 
   // 如果存在远程分支，则拉取远程分支
   await pullTargetBranch(target);
 
-  console.log(`开始把${sourceBranch}合并到${target}...`);
-
   try {
+    logWithSpinner(`开始把${sourceBranch}合并到${target}...`);
     // 合并当前分支到目标分支
     await shell.exec(`git merge ${sourceBranch}`);
-  } catch (error) {
-    console.log('合并存在冲突，需要手动处理。');
+    stopSpinner();
+    done('合并分支成功');
+  } catch (errorInstance) {
+    error('合并存在冲突，需要手动处理。');
     return;
   }
 
   // 如果需要推送到远程
   if (needPush) {
     try {
-      console.log('合并完毕，准备推送到远程仓库...');
+      logWithSpinner('合并完毕，准备推送到远程仓库...');
       await shell.exec('git push');
-    } catch (error) {
-      console.log('推送出现问题');
+      stopSpinner();
+      done('推送到远程分支成功');
+    } catch (errorInstance) {
+      error('推送出现问题');
       return;
     }
   }
 
   try {
+    info('准备切换回分支：dev');
     await shell.exec(`git checkout ${sourceBranch}`);
-    console.log('已切回分支：', sourceBranch);
-  } catch (error) {
-    console.log('切换回功能分支出现问题');
+    done('已切回分支');
+  } catch (errorInstance) {
+    error('切换回功能分支出现问题');
     return;
   }
 }
