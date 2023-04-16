4e24647397e3405c0f65a270635e562aaf7bd3cd
anthony <924358746@qq.com>
2023-04-16 09:21:54 +0800

    feat: 迁移mrm-preset项目到tool-cli

---
+++ packages/mrm-preset/README.md
@@ -0,0 +1,7 @@
+# @johnhom/mrm-preset
+
+根据mrm开发的项目配置文件的preset。
+
+包含的preset设置项：
+
+* beauty
---
+++ packages/mrm-preset/src/beauty/index.ts
@@ -0,0 +1,43 @@
+import { json, lines, install } from 'mrm-core';
+import _ from 'lodash';
+import { done, info, logWithSpinner, stopSpinner } from "@johnhom/cli-shared-utils";
+
+const task = async () => {
+  // 加入对ts代码的eslint 配置
+  const eslintPreset = '@johnhom/eslint-config-ts';
+  const legacyConfigFile = '.eslintrc.js';
+  const ignores = ['node_modules/'];
+  // 需要安装的依赖
+  const packages = ['eslint',  eslintPreset];
+
+  // eslintrc
+  const eslintrc = json(legacyConfigFile);
+  const hasCustomPreset = _.castArray(eslintrc.get('extends', [])).find((x) =>
+    x.startsWith(eslintPreset)
+  );
+  if (!hasCustomPreset) {
+    const presets = eslintrc.get('extends');
+    if (!presets) {
+      eslintrc.set('extends', eslintPreset);
+    } else {
+      eslintrc.set('extends', [eslintPreset, ..._.castArray(presets)]);
+    }
+  }
+  eslintrc.save();
+  info('生成.eslintrc.js');
+
+  // .eslintignore
+  lines('.eslintignore').add(ignores).save();
+
+  // 安装依赖 install会自动识别当前的项目的包管理工具
+  logWithSpinner('开始安装beauty配置的相关依赖...');
+  install(packages, { dev: true });
+  stopSpinner();
+  done('安装完成');
+
+  return Promise.resolve();
+};
+
+task.description = 'Adds ESLint and Prettier to the project';
+
+module.exports = task;
